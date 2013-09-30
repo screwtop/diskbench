3b4ba3f2b3403b0d1f75454828d98275e07cd05a
Charlie <root@hotblack.edwards.invalid>
2013-09-30 20:04:06 +1300

    Added sizeof() test for size_t.

--- sizeof.c
+++ sizeof.c
@@ -9,6 +9,7 @@ int main()
 	printf("sizeof(int) = %li\n", sizeof(int));
 	printf("sizeof(long) = %li\n", sizeof(long));
 	printf("sizeof(long long) = %li\n", sizeof(long long));
+	printf("sizeof(size_t) = %li\n", sizeof(size_t));
 	printf("sizeof(off_t) = %li\n", sizeof(off_t));
 	return 0;
 }
