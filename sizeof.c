bc5bfcfc587641cabcd32a563b225ebe1cd7c11e
Chris Edwards <screwtop.github@ripples.dyndns.org>
2021-01-20 12:53:03 +1300

    Add signedness check to sizeof program

--- sizeof.c
+++ sizeof.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <sys/types.h>	// For off_t
+#include <stdint.h>	// for intmax_t
+
+// For determining whether signed (https://stackoverflow.com/questions/1412197/how-do-i-tell-if-a-c-integer-variable-is-signed)
+#define ISVARSIGNED(V) ((V)-1<0 || -(V)-1<0)
+#define SIGNEDNESS(V) (ISVARSIGNED(V) ? "signed" : "unsigned")
 
 int main()
 {
-//	printf("sizeof(byte) = %li\n", sizeof(byte));
-	printf("sizeof(char) = %li\n", sizeof(char));
-	printf("sizeof(short) = %li\n", sizeof(short));
-	printf("sizeof(int) = %li\n", sizeof(int));
-	printf("sizeof(long) = %li\n", sizeof(long));
-	printf("sizeof(long long) = %li\n", sizeof(long long));
-	printf("sizeof(size_t) = %li\n", sizeof(size_t));
-	printf("sizeof(off_t) = %li\n", sizeof(off_t));
+//	byte b=0;
+	char c=0;
+	short s=0;
+	int i=0;
+	long l=0;
+	long long ll=0;
+	intmax_t imt=0;
+	size_t st=0;
+	off_t ot=0;
+
+//	printf("sizeof(byte) = %li, %s\n", sizeof(byte), SIGNEDNESS(b));
+	printf("sizeof(char) = %li, %s\n", sizeof(char), SIGNEDNESS(c));
+	printf("sizeof(short) = %li, %s\n", sizeof(short), SIGNEDNESS(s));
+	printf("sizeof(int) = %li, %s\n", sizeof(int), SIGNEDNESS(i));
+	printf("sizeof(long) = %li, %s\n", sizeof(long), SIGNEDNESS(l));
+	printf("sizeof(long long) = %li, %s\n", sizeof(long long), SIGNEDNESS(ll));
+	printf("sizeof(intmax_t) = %li, %s\n", sizeof(intmax_t), SIGNEDNESS(imt));
+	printf("sizeof(size_t) = %li, %s\n", sizeof(size_t), SIGNEDNESS(st));
+	printf("sizeof(off_t) = %li, %s\n", sizeof(off_t), SIGNEDNESS(ot));
+
 	return 0;
 }
